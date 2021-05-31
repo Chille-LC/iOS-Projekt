804537ba0aff0501fce098d2a374f4b217b2388f
MarkoLihter <lihter.marko@gmail.com>
2021-05-31 09:08:42 +0200

    1st commit

---
+++ OddJobsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OddJobsApp
+//
+//  Created by Mac Use on 31.05.2021..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
