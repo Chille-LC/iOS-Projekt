6ce7766f49206b2eaa14d824879731e2a83c0b47
Tomislav Pavkovic <tomislav.pavkovic@fer.hr>
2021-06-11 00:18:03 +0200

    all view controllers added

--- OddJobsApp/DataLayer/Models/JobCategory.swift
+++ OddJobsApp/DataLayer/Models/JobCategory.swift
@@ -5,7 +5,7 @@
 //  Created by Mac Use on 02.06.2021..
 //
 
-enum JobCategory: String, Decodable {
+enum JobCategory: String, Decodable, CaseIterable {
     case school = "SCHOOL"
     case fixInstall = "FIX AND INSTALL"
     case houseChores = "HOUSE CHORES"
